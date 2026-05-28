85b6905f4a530c30ae4b9baecbbfa5cb07eb6828
hicham-darw <185743161+hicham-darw@users.noreply.github.com>
2026-05-28 17:22:44 +0100

    add generate method

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,10 @@
-
+from llm_sdk.llm_sdk import Small_LLM_Model
 
 
 if __name__ == '__main__':
-    print("OK!, start CallMeMaybe!")
+    small_model = Small_LLM_Model()
+    print("small_model:", small_model)
+    print("OK!, start CallMeMaybe!\n")
+    small_model.encode("hello!, what's your name?")
+    tensor_ids = small_model.encode("Hello, What's your name?")
+    print("tensor_ids from encode:", tensor_ids)
