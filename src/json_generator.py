585f0cbc768c5cf35126997294e899ee9e8cb777
hicham-darw <hamoudihicham234@gmail.com>
2026-06-10 18:34:39 +0100

    add it as predict the first token must be OPEN_BRACE

--- src/json_generator.py
+++ src/json_generator.py
@@ -2,9 +2,10 @@ from typing import Any
 from src.processing_stage import ProcessingStage
 from transformers import AutoModel
 from src.validator import FunctionDefinitionSchema
-from src.state import JSONState, JSONField, ALLOWED
+from src.state import JSONState
 from llm_sdk.llm_sdk import Small_LLM_Model
 import torch
+import numpy as np
 
 
 class JSONGenerator(Small_LLM_Model, ProcessingStage):
@@ -14,38 +15,70 @@ class JSONGenerator(Small_LLM_Model, ProcessingStage):
 		super().__init__()
 		self.__json_results = list()
 		self.current_state = JSONState.IN_START
-		self.current_field = JSONField.START
+		# self.current_field = JSONField.START
 
 	def execute(self, data: Any) -> Any:
 		# must be load model first for tokenization
 		print("pipeline generator:")
-		allowed_functions_name: list[str] = [
-      		function.name for function in data['functions_definition']
-    	]
+		self.__allowed_functions_name: list[str] = [
+			function.name for function in data['functions_definition']
+		]
+
+		prompt = data['prompts'][0]		
+		clean_prompt = self.build_clean_prompt(data['functions_definition'], prompt)
+		input_ids = self.encode(clean_prompt)
 		
-  		# allowed function
-		print("allowed functions:", allowed_functions_name)
+		while self.current_state != JSONState.IN_END :
+			outputs = self._model(input_ids=input_ids) # (batch_size, sequence_length, config.vocab_size) score of each token in vocabulary before softmax
+			next_token_logits = outputs.logits[:, -1, :]
+			toks = []
+			allowed_tokens = self.get_allowed_tokens(self.current_state)
+			for token in allowed_tokens:
+				toks.append(self.encode(tok))
+			mask = np.full_like(next_token_logits, float("-inf"))
+			print("mask:", mask)
+			mask[:, toks] = 0 
+			print("mask:", mask)
+			print("type mask:", type(mask))
+			next_token_logits = next_token_logits + mask
+			next_token = torch.argmax(next_token_logits, dim=-1)
+			print(f"next_token: {next_token}")
+			input_ids = input_ids + next_token
+			# probabilities = torch.softmax(next_token_logits, dim=1)
+			# print("Probs:", probabilities)
+			self.current_state = JSONState.IN_END
 
+		print("decoding:", self.decode(next_token))
 		return None
-		# prompt = data['prompts'][0]		
-		# clean_prompt = self.build_clean_prompt(data['functions_definition'], prompt)
-
-		# input_ids = self.encode(clean_prompt)
-		# outputs = self._model(input_ids=input_ids)
-
 		# logits = outputs.logits[:, -1,:]
 		# allowed_tokens = self.get_allowed_tokens(
       	# 	self.current_state,
         # 	self.current_field,
         # )
-		# probs = torch.softmax(logits, dim=-1)
-		# # print("Probs:")
-		# # print(probs)
-		# # self.get_allowed_tokens(probs) # need contrained decoding with state and field
+		#  probs = torch.softmax(logits, dim=-1)
+		#  print("Probs:")
+		#  print(probs)
+		#  self.get_allowed_tokens(probs) # need contrained decoding with state and field
 
+	def get_allowed_tokens(self, state: JSONState) -> list[str]:
+		if state == JSONState.IN_OPEN_BRACE:
+			return ['{']
+		elif state == JSONState.IN_KEY:
+			return ["\"prompt\"", "\"name\"", "\"parameters\""]
+		elif state == JSONState.IN_DOUBLE_POINTS:
+			return [':']
+		elif state == JSONState.IN_VALUE:
+			return []
+		elif state == JSONState.IN_COMMA:
+			return [","]
+		elif state == JSONState.IN_CLOSE_BRACE:
+			return ["}"]
+		else:
+			return []
+	
 	#  outputs.past_key_values: cached attention from previous tokens
-	def get_allowed_tokens(self, state: JSONState, field: JSONField) -> list[str]:
-		return ALLOWED[state.name]
+	# def get_allowed_tokens(self, state: JSONState, field: JSONField) -> list[str]:
+	# 	return ALLOWED[state.name]
 
 	def build_clean_prompt(
     	self, functions_definition: list[FunctionDefinitionSchema], prompt: str
